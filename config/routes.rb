c9e22bafb2a53bf04cc8cf25861b2665cc70f135
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-02 19:40:10 -0300

    Redirecting to the log in page, so the unregistered user can try the app

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'devise/sessions'}
 
   #redirecting root route to users/sign_up
-  root :to => redirect('/users/sign_up')
-
+  root :to => redirect('/users/sign_in')
+  
 end
