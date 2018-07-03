42ba7f2f411adb4faf306e0c7194577add2c40a8
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-03 00:38:29 -0300

    Adding user data and styling it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,7 @@ class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    user = User.new
-    if user.sign_in_count = 0
-      :new_hair
-    else
-      :hairs
-    end
+    :hairs
   end
 
   def after_sign_out_path_for(resource_or_scope)
