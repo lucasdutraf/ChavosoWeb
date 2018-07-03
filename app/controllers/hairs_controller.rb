6fb453be94947446f9815d620249f311a896373e
Lucas Dutra <ldutra98@gmail.com>
2018-07-03 04:02:50 -0300

    trying to debug errors

--- app/controllers/hairs_controller.rb
+++ app/controllers/hairs_controller.rb
@@ -1,7 +1,12 @@
 class HairsController < ApplicationController
+  before_action only: [:show, :edit, :update, :destroy]
+
+  def index
+    @hairs = Hair.all
+  end
 
   def lenght=(new_lenght)
-    @lenght = new_lenght
+    write_attribute(:lenght, new_lenght)
   end
 
   def lenght
@@ -13,7 +18,52 @@ class HairsController < ApplicationController
   end
 
   def type=(new_type)
-    @type = new_type
+    write_attribute(:type, new_type)
+  end
+
+  def new
+    @hair = Hair.new
+  end
+
+  def update
+    respond_to do |format|
+      if @hair.update(hair_params)
+        format.html { redirect_to @hair, notice: 'UHUUUU' }
+        format.json { render :show, status: :ok, location: @hair }
+      else
+        format.html { render :edit }
+        format.json { render json: @hair.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def create
+    @hair = Hair.new(hair_params)
+    respond_to do |format|
+      if @hair.save
+        format.html { redirect_to @hair, notice: 'DEU CERTO' }
+        format.json { render :show, status: :created, location: @hair }
+      else
+        format.html { render :new }
+        format.json { render json: @hair.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # def set_hair
+  #   @hair = Hair.find(params[:id])
+  # end
+
+  def destroy
+    @hair.destroy
+    respond_to do |format|
+      format.html { redirect_to hairs_url, notice: 'hairs was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def hair_params
+    params.permit(:lenght, :type)
   end
 
 end
--- app/views/hairs/_hair.json.jbuilder
+++ app/views/hairs/_hair.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! hair, :id, :created_at, :updated_at
+json.extract! hair, :id, :created_at, :lenght, :type, :updated_at
 json.url hair_url(hair, format: :json)
