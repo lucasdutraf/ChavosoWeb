81fab4f6566f83558b4ed56508fb121094d2a354
Lucas Dutra <ldutra98@gmail.com>
2018-07-02 23:59:41 -0300

    Resolving db issues

--- db/migrate/20180701184712_create_hairs.rb
+++ db/migrate/20180701184712_create_hairs.rb
@@ -2,8 +2,8 @@ class CreateHairs < ActiveRecord::Migration[5.2]
   def change
     create_table :hairs do |t|
       t.string :lenght, null: false, default: " "
-      t.string :type, null: false, default: " "
-      
+      t.string :type,   null: false, default: " "
+
       t.timestamps
     end
   end
